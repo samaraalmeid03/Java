252d90dab434a70627705443e9a629a0e353b9e0
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-07-05 14:57:18 -0300

    Create PROBLEMA_MENOR_DE_TRES.java

---
+++ PROBLEMA_MENOR_DE_TRES.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, menor;
+		
+		System.out.print("Primeiro valor: ");
+		a = sc.nextInt();
+		System.out.print("Segundo valor: ");
+		b = sc.nextInt();
+		System.out.print("Terceiro valor: ");
+		c = sc.nextInt();
+		
+		if (a < b && a < c) {
+			menor = a;
+		}
+		else if (b < c) {
+			menor = b;
+		}
+		else {
+			menor = c;
+		}
+		
+		System.out.println("MENOR = " + menor);
+		
+		sc.close();	
+	}
+}
